d0c9af5964f2e006e098c48e290dd914cc37d024
fadhilsaheer <fadhilsaheer@gmail.com>
2021-08-09 22:17:03 +0530

    app bar

--- lib/screens/question_screen.dart
+++ lib/screens/question_screen.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/utils/colors.dart';
 
 class QuestionScreen extends StatelessWidget {
   const QuestionScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: appPrimaryColor,
+      appBar: AppBar(
+        title: Text('Question 1'),
+        backgroundColor: appSecondaryColor,
+        centerTitle: true,
+        elevation: 0.0,
+      ),
+    );
   }
 }
