586c32d0b1326afdd770252ce1d89cc7b19a347c
fadhilsaheer <fadhilsaheer@gmail.com>
2021-08-09 21:54:08 +0530

    question model

---
+++ lib/model/Question.dart
@@ -0,0 +1,11 @@
+class Question {
+  String question;
+  String answer;
+  List<String> options;
+
+  Question({
+    required this.question,
+    required this.answer,
+    required this.options,
+  });
+}
